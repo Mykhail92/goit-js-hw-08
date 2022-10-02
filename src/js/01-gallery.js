79c9ba7aa2c44734607b5d55d5e76442db9a0dc6
Mykhail92 <movemens1992@gmail.com>
2022-10-02 10:54:54 +0300

    install simpleLightBox with cdn and copletee task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,22 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+const refsGallery = document.querySelector(".gallery");
+console.log(SimpleLightbox);
 
-console.log(galleryItems);
+function createItem(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" title= "${description}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+const addCreateItem = createItem(galleryItems);
+refsGallery.insertAdjacentHTML("beforeend", addCreateItem);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
+console.dir(lightbox);
