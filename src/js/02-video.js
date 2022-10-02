962de025150c919ebfd02100a0bfd6ec9905421a
Mykhail92 <movemens1992@gmail.com>
2022-10-02 17:15:07 +0300

    complete task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
 
+
+import Player from "@vimeo/player";
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const throttle = require('lodash.throttle');
+
+
+player.on('timeupdate',  throttle( e => {
+    localStorage.setItem('videoplayer-current-time', e.seconds);
+    }, 1000)
+    );
+
+player
+.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+.catch(function (error) {
+    console.error(error)
+});
+
+console.dir(iframe);
+console.log(player);
